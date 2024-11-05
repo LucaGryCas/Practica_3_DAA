71c9dcaeb797bf457512289a00664c254f7a85eb
Alvarolpe <158449001+Alvarolpe@users.noreply.github.com>
2024-11-05 16:40:03 +0100

    Update algoritmo_prim.py

--- algoritmo_prim.py
+++ algoritmo_prim.py
@@ -3,22 +3,22 @@ import numpy as np
 def prim (M: np.matrix):
     n = len(M[1])
     mindist = np.repeat(0, n)
-    nearest = np.repeat(1, n)
-    T = set()
+    nearest = np.repeat(0, n)
+    T = set([])
 
-    for i in range(2,n):
-        mindist[i] = M[i,1]
+    for i in range(1,n):
+        mindist[i] = M[i,0]
 
     for i in range(n-1):
         min = np.inf
-        for j in range(2,n):
+        for j in range(1,n):
             if 0< mindist[j]<min:
                 min = mindist[j]
                 k = j
         a = (nearest[k],k,M[nearest[k],k])
-        T = T.union(set(a))
+        T.add(a)
         mindist[k] = 0
-        for j in range(2,n):
+        for j in range(1,n):
             if 0<M[j,k]<mindist[j]:
                 mindist[j] = M[j,k]
                 nearest[j] = k
